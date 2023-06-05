91a8b23cd58c2908eb063b9c829a5ed8df58773f
kiran1616 <kirankumar7163@gmail.com>
2023-06-05 16:54:18 +0530

    m

--- parameter-pull/run.sh
+++ parameter-pull/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-aws ssm describe-parameters --region us-east-1  --query "Parameters[*].Name" | xargs -n1 |sed -e 's/,//' | grep ${env}.${component}
+aws ssm describe-parameters --region us-east-1  --query "Parameters[*].Name" | xargs -n1 | sed -e 's/,//' | grep ${env}.${component} >/parameter-store/names
 
+for param  in 'cat /tmp/names'; do
+ VALUE=$( aws ssm get-parameters --region us-east-1 --names $param  --with-decryption --query Parameters[0].Value | sed 's/"//g' )
+ KEY=$(echo $param | awk -F . '{print $NF}')
+ echo export $KEY=\"$VALUE\" >>/parameter-store/params
+done
