dac0a38cd9707898d3b9de4535e693d0bb0ba266
kiran1616 <kirankumar7163@gmail.com>
2023-06-05 23:08:22 +0530

    m

---
+++ Schema/run.sh
@@ -0,0 +1,20 @@
+mkdir /code
+cd /code
+git clone https://github.com/kiran6055/${COMPONENT}
+
+# here is ${COMPONENT} is not working then try with out curly brases
+
+if [ $? -ne 0 ]; then
+  echo CLONE FAILED
+  exit1
+fi
+
+cd /code/schema
+if [ $SCHEMA_TYPE == "mongodb" ]; then
+  curl -L -o https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem
+  mongo --ssl --host ${DOCDB.ENDPOINT}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${DOCDB_USER} --password  ${DOCDB_PASS} < /code/schema/${COMPONENT}.js
+
+
+elif [ $SCHEMA_TYPE == "mysql" ]; then
+  echo
+fi
